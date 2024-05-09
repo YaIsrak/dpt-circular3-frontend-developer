e12822c0c07fa0da8967a92800603bed785432ce
YaIsrak <israkdreamstar@gmail.com>
2024-05-09 17:58:11 +0600

    Initial React Project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div>
+      <Button>CLick me</Button>
+      <h1>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
